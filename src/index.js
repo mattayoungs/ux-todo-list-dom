fc0000a28fd770919da2499c1c157025e91fe585
mattayoungs <mattayoungs@icloud.com>
2020-04-27 15:59:18 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,32 +1,50 @@
-
 let todos = [
-    {
-        completed: false,
-        description: "Take Jim to the hair salon"
-    },
-    {
-        completed: true,
-        description: "Drop off wedding invitation at mailbox"
-    },
-    {
-        completed: false,
-        description: "Pick up the cake"
-    },
-    {
-        completed: false,
-        description: "Call the caterers"
-    }
+  {
+    completed: false,
+    description: "Take Jim to the hair salon",
+  },
+  {
+    completed: true,
+    description: "Drop off wedding invitation at mailbox",
+  },
+  {
+    completed: false,
+    description: "Pick up the cake",
+  },
+  {
+    completed: false,
+    description: "Call the caterers",
+  },
 ];
 
 function renderTodoApp() {
-    let app = document.querySelector("#app")
-    let h1 = document.createElement("h1")
-    // your code here
+  let app = document.querySelector("#app");
+  let h1 = document.createElement("h1");
+  // your code here
+  let list = document.createElement("ul");
 
-    h1.innerText = "Todo List"
+  for (let item of todos) {
+    li = document.createElement("li");
+    li.style.listStyle = "none";
+    li.innerHTML = `
+      <input type="checkbox" name="item" id="item" />
+      <label for="item">${item.description}</label>
+      `;
+    if (item.completed === true) {
+      li.innerHTML = `
+      <input checked type="checkbox" name="item" id="item" />
+      <label for="item">${item.description}</label>
+      `;
+    }
+    //CHECK TO SEE IF COMPLETED IS T F
+    // if t checkbox is checked
+    list.appendChild(li);
+  }
+  h1.innerText = "Todo List";
 
-    app.appendChild(h1)
-    // and maybe some here
+  app.appendChild(h1);
+  app.appendChild(list);
+  // and maybe some here
 }
 
-renderTodoApp()
+renderTodoApp();
